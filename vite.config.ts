c82c55d40c5350bc102ef8cd2d91b90748fca114
Sokna <sokna@example.com>
2026-06-11 19:02:31 +0700

    fix: point API and Pusher to backend URL

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://store-car-seven.vercel.app',
+        target: 'https://admin-car-2dr5.vercel.app',
         changeOrigin: true,
       },
     },
